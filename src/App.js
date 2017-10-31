9b9521b8fd447d4ce5f3b4c758101e936b2cbbe3
Vinay Prajapati <vinayprajapati107@gmail.com>
2017-10-31 22:19:55 +0530

    First sample react-project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import './App.css';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+          <Button>Cloack</Button>
+          <Jumbotron></Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default App;
